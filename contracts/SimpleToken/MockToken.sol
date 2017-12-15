43e1d7dc814cee969830d4f9c22cb00ff2498d63
Jason Banks <banks@simpletoken.org>
2017-12-15 09:02:39 +0100

    Contracts: add and test MockToken. Resolves #70.

---
+++ contracts/SimpleToken/MockToken.sol
@@ -0,0 +1,44 @@
+pragma solidity ^0.4.17;
+
+// ----------------------------------------------------------------------------
+// Mock Token Contract
+//
+// Copyright (c) 2017 OpenST Ltd.
+// https://simpletoken.org/
+//
+// The MIT Licence.
+// ----------------------------------------------------------------------------
+
+
+import "./ERC20Token.sol";
+import "./SimpleTokenConfig.sol";
+import "./OpsManaged.sol";
+
+contract MockToken is ERC20Token, OpsManaged, SimpleTokenConfig {
+
+    bool public finalized;
+
+
+    // Events
+    event Finalized();
+
+    function MockToken() public
+        ERC20Token("MOCK", "Mock Token", TOKEN_DECIMALS, TOKENS_MAX)
+        OpsManaged()
+        { }
+
+    // Finalize functionality retained because it is expected by platform scripts
+    function finalize() external onlyAdmin returns (bool success) {
+        require(!finalized);
+
+        finalized = true;
+
+        Finalized();
+
+        return true;
+    }
+
+    function remove() public onlyOwner {
+        selfdestruct(msg.sender);
+    }    
+}
