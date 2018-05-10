bc8a7c677b512666acbb5f45efa350246f0710f8
abhay-pepo <abhay@pepo.com>
2018-05-10 13:18:44 +0530

    merkle proofs

--- tools/merkle_proof.js
+++ tools/merkle_proof.js
@@ -16,13 +16,14 @@ const blockHash = "d1d2effa9d957d373c5968064d318f271de34f7b5fc7c87089598eadb84c7
 ;
 
 Web3.providers.HttpProvider.prototype.sendAsync = Web3.providers.HttpProvider.prototype.send;
-// const epObjectWithoutChainData = new EP(new Web3.providers.HttpProvider("http://127.0.0.1:9546"))
-//   , epObjectWithChainData = new EP(new Web3.providers.HttpProvider("http://127.0.0.1:9546"), blockHash, chainDataPath)
-// ;
 
 const merkleProof = {
 
   receiptProofWithChainData: function(){
+    const epObjectWithoutChainData = new EP(new Web3.providers.HttpProvider("http://127.0.0.1:9546"))
+      , epObjectWithChainData = new EP(new Web3.providers.HttpProvider("http://127.0.0.1:9546"), blockHash, chainDataPath)
+    ;
+
     epObjectWithChainData.getReceiptProof(txHash).then(function(proof){
       console.log("\n===========Starting receiptProofWithChainData===========\n");
       console.log("\n===========receiptProofWithChainData===========\n", proof);
@@ -31,6 +32,10 @@ const merkleProof = {
   },
 
   logProofWithChainData: function(){
+    const epObjectWithoutChainData = new EP(new Web3.providers.HttpProvider("http://127.0.0.1:9546"))
+      , epObjectWithChainData = new EP(new Web3.providers.HttpProvider("http://127.0.0.1:9546"), blockHash, chainDataPath)
+    ;
+
     epObjectWithChainData.getLogProof(txHash, 0).then(function(proof){
       console.log("\n===========Starting logProofWithChainData===========\n");
       console.log("\n===========logProofWithChainData===========\n", proof);
@@ -39,6 +44,10 @@ const merkleProof = {
   },
 
   transactionProofWithChainData: function(){
+    const epObjectWithoutChainData = new EP(new Web3.providers.HttpProvider("http://127.0.0.1:9546"))
+      , epObjectWithChainData = new EP(new Web3.providers.HttpProvider("http://127.0.0.1:9546"), blockHash, chainDataPath)
+    ;
+
     epObjectWithChainData.getTransactionProof(txHash).then(function(proof){
       console.log("\n===========Starting transactionProofWithChainData===========\n");
       console.log("\n===========transactionProofWithChainData===========\n", proof);
@@ -47,6 +56,10 @@ const merkleProof = {
   },
 
   accountProofWithChainData: function(){
+    const epObjectWithoutChainData = new EP(new Web3.providers.HttpProvider("http://127.0.0.1:9546"))
+      , epObjectWithChainData = new EP(new Web3.providers.HttpProvider("http://127.0.0.1:9546"), blockHash, chainDataPath)
+    ;
+
     console.log("\nStarting accountProofWithChainData");
     epObjectWithChainData.getAccountProof(accountAddress).then(async function(proof){
       console.log("\n===========Starting accountProofWithChainData===========\n");
@@ -72,36 +85,63 @@ const merkleProof = {
   //   to: null,
   //   transactionHash: '0x2f159e7ad8a1a06baadc00cfd4cb1fda398d86e8471c9a8c76d0e010a4df84bf',
   //   transactionIndex: 0 }
-  storageProofWithChainData: function(){
-    var storageContractAddress = 'DD0339B562F83209e2DB7812abAAD399d5059c67'
+  storageProofWithChainDataForVariableValue: function(){
+    var storageContractAddress = 'dd0339b562f83209e2db7812abaad399d5059c67'
       , blockHash = 'af85800048e1aed4efd9665b5ae37329ee2eb72095cc8a8cfbd2c881f0e24210'
-      , storageContractDeployer = '0xb43190ee505e335a71ab46bf6057a8e023a55c11'
+      , position = 0x0
+      , positionContractValue = 1234
     ;
     var epObject = new EP(new Web3.providers.HttpProvider("http://127.0.0.1:9546"), blockHash, chainDataPath);
-    epObject.getStorageProof(storageContractAddress, 0x0).then(async function(proof){
-      console.log("\n===========Starting storageProofWithChainData===========\n");
+    epObject.getStorageProof(storageContractAddress, position).then(async function(proof){
+      console.log("\n===========Starting storageProofWithChainDataForVariableValue===========\n");
       console.log("\n===========storageProofWithChainData===========\n", proof);
-      console.log("\n===========Ending storageProofWithChainData===========\n");
-      console.log("\n===========Starting verifyStorageProofWithChainData===========\n");
+      console.log("\n===========Ending storageProofWithChainDataForVariableValue===========\n");
       var verifyResult = await EP.storageAtIndex(
         proof.storageIndex,
-        Buffer.from('1234','hex'),
+        positionContractValue,
         proof.storageParentNodes,
         proof.address,
         proof.accountParentNodes,
         proof.header,
         proof.blockHash);
-      // var verifyResult = await EP.storageMapping(
-      //   proof.storageIndex,
-      //   proof.mappings,
-      //   proof.value,
-      //   proof.storageParentNodes,
-      //   proof.address,
-      //   proof.accountParentNodes,
-      //   proof.header,
-      //   proof.blockHash);
+      console.log("storageProofWithChainDataForVariableValue verifyAccountProof Result:", verifyResult);
+    });
+  },
+
+  // Storage Proof Data
+  // { blockHash: '0xaf85800048e1aed4efd9665b5ae37329ee2eb72095cc8a8cfbd2c881f0e24210',
+  //   blockNumber: 4229,
+  //   contractAddress: '0xDD0339B562F83209e2DB7812abAAD399d5059c67',
+  //   cumulativeGasUsed: 153738,
+  //   from: '0xb43190ee505e335a71ab46bf6057a8e023a55c11',
+  //   gasUsed: 153738,
+  //   logs: [],
+  //   logsBloom: '0x00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000',
+  //   status: '0x1',
+  //   to: null,
+  //   transactionHash: '0x2f159e7ad8a1a06baadc00cfd4cb1fda398d86e8471c9a8c76d0e010a4df84bf',
+  //   transactionIndex: 0 }
+  storageProofWithChainDataForMappingKey: function(){
+    var storageContractAddress = 'dd0339b562f83209e2db7812abaad399d5059c67'
+      , blockHash = 'af85800048e1aed4efd9665b5ae37329ee2eb72095cc8a8cfbd2c881f0e24210'
+      , storageContractDeployerMappingKey = 'b43190ee505e335a71ab46bf6057a8e023a55c11'
+      , position = '01'
+    ;
+    var epObject = new EP(new Web3.providers.HttpProvider("http://127.0.0.1:9546"), blockHash, chainDataPath);
+    epObject.getStorageProof(storageContractAddress, position, storageContractDeployerMappingKey).then(async function(proof){
+      console.log("\n===========Starting storageProofWithChainDataForMappingKey===========\n");
+      console.log("\n===========storageProofWithChainDataForMappingKey===========\n", proof);
+      console.log("\n===========Ending storageProofWithChainDataForMappingKey===========\n");
+      var verifyResult = await EP.storageMapping(
+        proof.storageIndex,
+        proof.mappings,
+        proof.value,
+        proof.storageParentNodes,
+        proof.address,
+        proof.accountParentNodes,
+        proof.header,
+        proof.blockHash);
       console.log("verifyAccountProof Result:", verifyResult);
-      console.log("\n===========Ending verifyStorageProofWithChainData===========\n");
     });
   },
 
@@ -109,12 +149,13 @@ const merkleProof = {
   perform: function(){
     const oThis = this
     ;
-    // oThis.transactionProofWithChainData();
-    // oThis.receiptProofWithChainData();
-    // oThis.logProofWithChainData();
-    // oThis.transactionProofWithChainData();
-    // oThis.accountProofWithChainData();
-    oThis.storageProofWithChainData();
+    oThis.transactionProofWithChainData();
+    oThis.receiptProofWithChainData();
+    oThis.logProofWithChainData();
+    oThis.transactionProofWithChainData();
+    oThis.accountProofWithChainData();
+    oThis.storageProofWithChainDataForVariableValue();
+    oThis.storageProofWithChainDataForMappingKey();
   }
 
 
