7c52b37ec021d2095bbd30ffb36bb693b1e9e592
Sarvesh Jain <sarvesh@ost.com>
2018-06-07 20:35:24 +0530

    Added Test cases for library and desired require statement for each library function

--- contracts/AddressArrayLib.sol
+++ contracts/AddressArrayLib.sol
@@ -34,20 +34,27 @@ library AddressArrayLib {
      *  @Returns: new length of array
      */
     function add(AddressArray storage array, address value) internal view returns (uint){
+        require(value != address(0), "value should be non zero");
+
         array.addresses.push(value);
         return array.addresses.length;
     }
 
-    /*  @title -  Remove element from array by value
+    /*  @title -  Remove first occurrence of element from array by value
      *  @Param - array
      *  @Param - value which will be removed from
      *  @Returns: new length of array
      */
     function removeByValue(AddressArray storage array, address value) internal view returns (uint) {
+        require(value != address(0), "value should be non zero");
+
         uint256 index;
         bool isElementPresent;
+
         (isElementPresent, index) = find(array, value);
+
         require(isElementPresent, "Element doesn't exist in Array");
+
         removeByIndex(array, index);
         return array.addresses.length;
     }
@@ -59,30 +66,38 @@ library AddressArrayLib {
      *  @Returns - index of element
      */
     function find(AddressArray storage array, address value) internal view returns (bool, uint) {
+        require(value != address(0), "value should be non zero");
+
         uint256 i = 0;
         bool isElementPresent = false;
-        while (!(isElementPresent = (array.addresses[i] == value)) && i < array.addresses.length) {
+
+        while (i < array.addresses.length && !(isElementPresent = (array.addresses[i] == value))) {
             i++;
         }
         return (isElementPresent, i);
     }
-    /*  @title - Remove element in array by index
-     *  @Param - array
-     *  @Param - index of element
-     *  @Returns - new length of array
-     */
-    function removeByIndex(AddressArray storage array, uint256 i) private view {
-        while (i < array.addresses.length - 1) {
-            array.addresses[i] = array.addresses[i + 1];
-            i++;
-        }
-        array.addresses.length--;
-    }
+
     /*  @title - length of array
      *  @Param - array
      *  @Returns - length of array
      */
     function length(AddressArray storage array) internal view returns(uint){
         return array.addresses.length;
+    }
+
+    /*  @title - Remove element in array by index
+     *  @Param - array
+     *  @Param - index of element
+     *  @Returns - new length of array
+     */
+    function removeByIndex(AddressArray storage array, uint256 i) private view {
+        require(i >= 0, "Index should be greater than zero");
+        require(i < array.addresses.length, "Index should be less than length");
+
+        while (i < array.addresses.length - 1) {
+            array.addresses[i] = array.addresses[i + 1];
+            i++;
+        }
+        array.addresses.length--;
     }
 }
---
+++ test/AddressArrayLibMockTest.js
@@ -0,0 +1,118 @@
+const AddressArrayLibMock = artifacts.require('./AddressArrayLibMock.sol');
+const Utils = require('./lib/utils.js');
+const BigNumber = require('bignumber.js');
+
+contract('AddressArrayLib', function (accounts) {
+
+
+  describe(' Add to array', async () => {
+
+    let contract;
+    before(async () => {
+      contract = await AddressArrayLibMock.new();
+
+    });
+
+    it('Should be able to add item to array', async () => {
+      let initialLength = await contract.length.call();
+      await contract.add('0x345632343');
+      let finalLength = await contract.length.call();
+      assert.equal(initialLength.add(1).eq(finalLength), true);
+    });
+
+    it('Should be able to  multiple  item to array', async () => {
+      let initialLength = await contract.length.call();
+      await contract.add('0x4547575565566');
+      await contract.add('0x4686377547555');
+      await contract.add('0x5679952134567');
+      let finalLength = await contract.length.call();
+      assert.equal(initialLength.add(3).eq(finalLength), true);
+    });
+
+    it('Should be able to add  invalid item to array', async () => {
+      await Utils.expectThrow(contract.add(0));
+    });
+
+  });
+
+
+  describe(' find from array', async () => {
+
+    let contract;
+    before(async () => {
+      contract = await AddressArrayLibMock.new();
+      await contract.add('0x4547575565566');
+      await contract.add('0x4686377547555');
+      await contract.add('0x5679952134567');
+
+    });
+
+    it('Should be able to find item in array', async () => {
+      let findResult = await contract.find.call('0x4547575565566');
+      assert.equal(findResult[0], true);
+      assert.equal(findResult[1].eq(new BigNumber(0)), true);
+
+    });
+
+    it('Should return false if element doesnot exist in the array', async () => {
+      let findResult = await contract.find.call('0x470503050550');
+      assert.equal(findResult[0], false);
+    });
+
+    it('Should return false if invalid element is passed in find operation', async () => {
+      await Utils.expectThrow(contract.find.call(0));
+    });
+
+    it('Should return false if  element is passed in find operation in an empty array', async () => {
+      let blankArray = await AddressArrayLibMock.new();
+      let findResult = await blankArray.find.call('0x123456789');
+      assert.equal(findResult[0], false);
+    });
+
+
+  });
+
+  describe('remove from array by value', async () => {
+
+    let contract;
+    before(async () => {
+      contract = await AddressArrayLibMock.new();
+      await contract.add('0x4547575565566');
+      await contract.add('0x4686377547555');
+      await contract.add('0x5679952134567');
+
+    });
+
+    it('Should be able to remove item in array', async () => {
+      let initialLength = await contract.length.call();
+      let finalLength = await contract.removeByValue.call('0x4547575565566');
+      assert.equal(initialLength.sub(1).eq(finalLength), true);
+
+    });
+
+    it('Should be able to remove first occurrence  array if value exist many times', async () => {
+      await contract.add('0x4547575565566');
+      let initialLength = await contract.length.call();
+      let finalLength = await contract.removeByValue.call('0x4547575565566');
+      assert.equal(initialLength.sub(1).eq(finalLength), true);
+      let findResult = await contract.find.call('0x4547575565566');
+      assert.equal(findResult[0], true);
+    });
+
+    it('Should throw exception if values is not in array', async () => {
+      await Utils.expectThrow(contract.removeByValue.call('0x123456789'));
+    });
+
+    it('Should throw exception if array is blank', async () => {
+      let blankArray = await AddressArrayLibMock.new();
+      await Utils.expectThrow(blankArray.removeByValue.call('0x123456789'));
+    });
+
+    it('Should throw exception if invalid value is passed', async () => {
+      let blankArray = await AddressArrayLibMock.new();
+      await Utils.expectThrow(blankArray.removeByValue.call(0));
+    });
+
+  })
+
+});
