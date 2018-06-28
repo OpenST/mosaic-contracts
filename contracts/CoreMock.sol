820d8908b97fe224c64016575258a94e6574ae25
Gulshan Vasnani <gulshan@ost.com>
2018-06-28 15:16:34 +0530

    Added mocking of Core.sol.gulshan/MOSA-63

---
+++ contracts/CoreMock.sol
@@ -0,0 +1,43 @@
+pragma solidity ^0.4.23;
+import "./Core.sol";
+
+contract CoreMock is Core{
+
+    /**
+     * @notice Contract constructor
+     *
+     * @dev bytes32ToBytes is util contract method
+     *
+     * @param _registrar registrar address
+     * @param _chainIdOrigin origin chain id
+     * @param _chainIdRemote remote chain id
+     * @param _openSTRemote remote openSTUtility/openSTValue contract address
+     */
+    constructor(
+        address _registrar,
+        uint256 _chainIdOrigin,
+        uint256 _chainIdRemote,
+        address _openSTRemote,
+        WorkersInterface _workers)
+        Core(_registrar, _chainIdOrigin, _chainIdRemote, _openSTRemote, _workers) public { }
+
+    /**
+	 *	@notice public view function getStorageRoot
+	 *
+	 *	@param _blockHeight block height for which storage root is needed
+	 *
+	 *	@return bytes32 storage root
+	 */
+    function getStorageRoot(
+        uint256 _blockHeight)
+    public
+    view
+    returns (bytes32 /* storage root */)
+    {
+        return keccak256(_blockHeight);
+    }
+
+
+
+
+}
