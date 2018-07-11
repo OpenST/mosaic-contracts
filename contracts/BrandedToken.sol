e424e29ef4d749d3e71f0c26c2f26a86ef1ed0de
Pranay Valson <pranay.valson@gmail.com>
2018-07-11 13:57:23 +0200

    contracts: updates BrandedToken comments

--- contracts/BrandedToken.sol
+++ contracts/BrandedToken.sol
@@ -24,26 +24,36 @@ pragma solidity ^0.4.23;
 
 import "./SafeMath.sol";
 
-// utility chain contracts
+/** utility chain contracts */
 import "./EIP20Token.sol";
 import "./UtilityTokenAbstract.sol";
 
 
-/// @dev  Branded Token is an EIP20 token minted by staking Simple Token
-///       on Ethereum mainnet. Branded tokens are designed to be used
-///       within a (decentralised) application and support:
-///        - smart contract controlled password reset for users who don't
-///          yet (hard-spoon FTW) manage their own private keys (+v0.9.2)
-///        - soft-exit for a user to redeem their equivalent part of the 
-///          Simple Token stake on Ethereum mainnet
-///        - hard-exit for all users if the utility chain halts to reclaim
-///          their equivalent part of the Simple Token stake
-///          on Ethereum (before v1.0)
+/**
+ *  @title BrandedToken contract which implements EIP20Token, UtilityTokenAbstract. 
+ *
+ *  @notice Branded Token is an EIP20 token minted by staking Simple Token
+ *          on Ethereum mainnet.  
+ *
+ *  @dev Branded tokens are designed to be used within a (decentralised) application and support:
+ *       - Smart contract controlled password reset for users who don't
+ *         yet (hard-spoon FTW) manage their own private keys (+v0.9.2).
+ *       - Soft-exit for a user to redeem their equivalent part of the 
+ *         Simple Token stake on Ethereum mainnet.
+ *       - Hard-exit for all users if the utility chain halts to reclaim
+ *         their equivalent part of the Simple Token stake
+ *         on Ethereum (before v1.0).
+ */
 contract BrandedToken is EIP20Token, UtilityTokenAbstract {
     using SafeMath for uint256;
 
-    /*
-     *  Public functions
+    /* Public functions */
+
+    /**
+     *  @notice Contract Constructor. 
+     *
+     *  @dev Creates an EIP20Token and a UtilityTokenAbstract contract with arguments 
+     *       passed in the contract constructor
      */
     constructor(
         bytes32 _uuid,
@@ -66,6 +76,17 @@ contract BrandedToken is EIP20Token, UtilityTokenAbstract {
         _conversionRateDecimals)
         { }
 
+    /**
+     *  @notice Public function claim
+     *
+     *  @dev Calls on claimInteral which returns amount of utility
+     *       tokens to transfer for _beneficiary address
+     *
+     *  @param _beneficiary Address of the utility tokens beneficiary
+     *
+     *  @return True if claim of utility tokens for beneficiary address is successful, 
+     *          false otherwise.
+     */
     function claim(
         address _beneficiary)
         public
@@ -76,6 +97,17 @@ contract BrandedToken is EIP20Token, UtilityTokenAbstract {
         return claimEIP20(_beneficiary, amount);
     }
 
+    /**
+     *  @notice Public function mintEIP20.
+     *
+     *  @dev Only callable by openSTProtocol contract. Adds _amount of utility tokens to 
+     *       be claimed for a _beneficiary address.
+     *
+     *  @param _beneficiary Address of beneficiary.
+     *  @param _amount Amount of utility tokens to mint.
+     *
+     *  @return True if mint is successful, false otherwise.
+     */
     function mint(
         address _beneficiary,
         uint256 _amount)
@@ -88,6 +120,16 @@ contract BrandedToken is EIP20Token, UtilityTokenAbstract {
         return mintInternal(_beneficiary, _amount);
     }
 
+    /**
+     *  @notice Public function burn.
+     *
+     *  @dev Only callable by openSTProtocol contract. Implements a burn function to permit 
+     *       msg.sender to reduce its balance, which also reduces tokenTotalSupply.
+     *
+     *  @param _value Amount of tokens to burn.
+     *
+     *  @return True if burn is successful, false otherwise.
+     */
     function burn(
         address _burner,
         uint256 _amount)
