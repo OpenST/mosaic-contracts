77b38ab0a66d73ea9fdf5b7adc2ae535a0ca29d2
Pranay Valson <pranay.valson@gmail.com>
2018-07-12 17:59:13 +0200

    contracts: updates SimpleStake comments

--- contracts/SimpleStake.sol
+++ contracts/SimpleStake.sol
@@ -25,32 +25,37 @@ import "./EIP20Interface.sol";
 import "./SafeMath.sol";
 import "./ProtocolVersioned.sol";
 
-/// @title SimpleStake - stakes the value of an EIP20 token on Ethereum
-///        for a utility token on the OpenST platform
-/// @author OpenST Ltd.
+/**
+ *  @title SimpleStake contract which implements ProtocolVersioned
+ *
+ *  @notice Stakes the value of an EIP20 token on Ethereum
+ *          for a utility token on the OpenST platform.
+ */
 contract SimpleStake is ProtocolVersioned {
 	using SafeMath for uint256;
 
-	/*
-	 *  Events
-	 */
+	/** Events */
+
 	event ReleasedStake(address indexed _protocol, address indexed _to, uint256 _amount);
 
-	/*
-	 *  Storage
-	 */
-	/// EIP20 token contract that can be staked
+	/** Storage */
+
+	/** EIP20 token contract that can be staked */
 	EIP20Interface public eip20Token;
-	/// UUID for the utility token
+	/** UUID for the utility token */
 	bytes32 public uuid;
 
-	/*
-	 *  Public functions
+	/** Public functions */
+
+	/**
+	 *  @notice Contract constructor.
+	 *
+	 *  @dev Sets the protocol and the EIP20 token to stake.
+	 *
+	 *  @param _eip20Token EIP20 token that will be staked.
+	 *  @param _openSTProtocol OpenSTProtocol contract that governs staking.
+	 *  @param _uuid Unique Universal Identifier of the registered utility token.
 	 */
-	/// @dev Contract constructor sets the protocol and the EIP20 token to stake
-	/// @param _eip20Token EIP20 token that will be staked
-	/// @param _openSTProtocol OpenSTProtocol contract that governs staking
-	/// @param _uuid Unique Universal Identifier of the registered utility token
 	constructor(
 		EIP20Interface _eip20Token,
 		address _openSTProtocol,
@@ -62,13 +67,19 @@ contract SimpleStake is ProtocolVersioned {
 		uuid = _uuid;
 	}
 
-	/// @dev Allows the protocol to release the staked amount
-	///      into provided address.
-	///      The protocol MUST be a contract that sets the rules
-	///      on how the stake can be released and to who.
-	///      The protocol takes the role of an "owner" of the stake.
-	/// @param _to Beneficiary of the amount of the stake
-	/// @param _amount Amount of stake to release to beneficiary
+	/**
+	 *  @notice Public function releaseTo
+	 *
+	 *  @dev Callable only by protocol. Allows the protocol to release the staked amount
+	 *       into provided address. The protocol MUST be a contract that sets the rules
+	 *       on how the stake can be released and to who.
+	 *       The protocol takes the role of an "owner" of the stake.
+	 *
+	 *  @param _to Beneficiary of the amount of the stake.
+	 *  @param _amount Amount of stake to release to beneficiary.
+	 *
+	 *	@return bool True if stake is released to beneficiary, false otherwise.
+	 */
 	function releaseTo(address _to, uint256 _amount) 
 		public 
 		onlyProtocol
@@ -82,14 +93,19 @@ contract SimpleStake is ProtocolVersioned {
 		return true;
 	}
 
-	/*
-     * Web3 call functions
+	/** Web3 call functions */
+
+	/**
+	 *	@notice Public view function getTotalStake
+	 *
+     *  @dev Total stake is the balance of the staking contract
+     *       accidental transfers directly to SimpleStake bypassing
+     *       the OpenST protocol will not mint new utility tokens,
+     *       but will add to the total stake,
+     *       (accidental) donations can not be prevented.
+     *
+     *	@return uint256 Total staked amount.
      */
-    /// @dev total stake is the balance of the staking contract
-    ///      accidental transfers directly to SimpleStake bypassing
-    ///      the OpenST protocol will not mint new utility tokens,
-    ///      but will add to the total stake.
-    ///      (accidental) donations can not be prevented
     function getTotalStake()
     	public
     	view
