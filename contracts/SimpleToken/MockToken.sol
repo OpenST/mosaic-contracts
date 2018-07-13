1c4fd0571cb65fb2076815964c5e62c7adfffd21
Pranay Valson <pranay.valson@gmail.com>
2018-07-13 19:57:25 +0200

    contracts: updates mockToken

--- contracts/SimpleToken/MockToken.sol
+++ contracts/SimpleToken/MockToken.sol
@@ -10,30 +10,85 @@ pragma solidity ^0.4.23;
 // ----------------------------------------------------------------------------
 
 
-import "./ERC20Token.sol";
-import "./SimpleTokenConfig.sol";
+import "./EIP20Token.sol";
+import "./MockTokenConfig.sol";
 import "./OpsManaged.sol";
 
-contract MockToken is ERC20Token, OpsManaged, SimpleTokenConfig {
+contract MockToken is EIP20Token, OpsManaged, MockTokenConfig {
+    using SafeMath for uint256;
 
-    bool public finalized;
+    string  private tokenName;
+    string  private tokenSymbol;
+    uint8   private tokenDecimals;
+    uint256 internal tokenTotalSupply;
 
-
-    // Events
-    event Finalized();
-
+    mapping(address => uint256) balances;
+    mapping(address => mapping (address => uint256)) allowed;
+    
     constructor() public
-        ERC20Token("MOCK", "Mock Token", TOKEN_DECIMALS, TOKENS_MAX)
+        EIP20Token(TOKEN_SYMBOL, TOKEN_NAME, TOKEN_DECIMALS)
         OpsManaged()
         { }
 
-    // Finalize functionality retained because it is expected by platform scripts
-    function finalize() external onlyAdmin returns (bool success) {
-        require(!finalized);
+    function name() public view returns (string) {
+        return tokenName;
+    }
 
-        finalized = true;
 
-        emit Finalized();
+    function symbol() public view returns (string) {
+        return tokenSymbol;
+    }
+
+
+    function decimals() public view returns (uint8) {
+        return tokenDecimals;
+    }
+
+
+    function totalSupply() public view returns (uint256) {
+        return tokenTotalSupply;
+    }
+
+
+    function balanceOf(address _owner) public view returns (uint256) {
+        return balances[_owner];
+    }
+
+
+    function allowance(address _owner, address _spender) public view returns (uint256 remaining) {
+        return allowed[_owner][_spender];
+    }
+
+
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        // According to the EIP20 spec, "transfers of 0 values MUST be treated as normal
+        // transfers and fire the Transfer event".
+        // Also, should throw if not enough balance. This is taken care of by SafeMath.
+        balances[msg.sender] = balances[msg.sender].sub(_value);
+        balances[_to] = balances[_to].add(_value);
+
+        emit Transfer(msg.sender, _to, _value);
+
+        return true;
+    }
+
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        balances[_from] = balances[_from].sub(_value);
+        allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
+        balances[_to] = balances[_to].add(_value);
+
+        emit Transfer(_from, _to, _value);
+
+        return true;
+    }
+
+
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+
+        allowed[msg.sender][_spender] = _value;
+
+        emit Approval(msg.sender, _spender, _value);
 
         return true;
     }
