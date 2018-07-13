58c32a7fe2ab63b23915c193189557e9e0ca1406
abhay <abhay@ost.com>
2018-07-13 20:46:37 +0530

    [MOSA-31] stake changed to stakeItem resolves gh-192

--- contracts/OpenSTValue.sol
+++ contracts/OpenSTValue.sol
@@ -288,7 +288,7 @@ contract OpenSTValue is OpsManaged, Hasher {
         require(_stakingIntentHash != "");
 
         Stake storage stakeItem = stakes[_stakingIntentHash];
-        UtilityToken storage utilityToken = utilityTokens[stake.uuid];
+        UtilityToken storage utilityToken = utilityTokens[stakeItem.uuid];
         // if the staking account is set to a non-zero address,
         // then all transactions have come (from/over) the staking account
         if (utilityToken.stakingAccount != address(0)) require(msg.sender == utilityToken.stakingAccount);
