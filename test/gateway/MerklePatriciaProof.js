e4b617a4b14e4e1dfd76ed0694fece2be9c919e9
Pranay Valson <pranay.valson@gmail.com>
2018-08-14 17:00:54 +0200

    [OMC-54] contracts, tests: adds tests for merklePatriciaProof, modifies fn call #241

--- test/gateway/MerklePatriciaProof.js
+++ test/gateway/MerklePatriciaProof.js
@@ -19,6 +19,11 @@ contract('MerklePatriciaProof', function(accounts) {
             assert.equal(proofStatus,false);
         })
 
+        it('Should fail when encoded path is empty', async () => {
+            let proofStatus = await merkleMock.verifyAccount.call(proofJson.account.sha3EncodedAccount, "", proofJson.account.rlpParentNodes, proofJson.account.stateRoot);
+            assert.equal(proofStatus,false);
+        })
+
         it('Should fail when state root is incorrect', async () => {
             let proofStatus = await merkleMock.verifyAccount.call(proofJson.account.sha3EncodedAccount, proofJson.account.hashedPath, proofJson.account.rlpParentNodes, "0x58810687b84d5bddc1e9e68b2733caa4a8c6c9e7dd5d0b2f9c28b4bbf5c6f851");
             assert.equal(proofStatus,false);
@@ -39,52 +44,57 @@ contract('MerklePatriciaProof', function(accounts) {
 
         it('Should pass for valid variable storage',async () =>{
             let proofStatus = await merkleMock.verifyStorage.call(proofJson.storage.variable.sha3ValueAtStorage, proofJson.storage.variable.path, proofJson.storage.variable.rlpParentNodes, proofJson.storage.variable.storageRoot);
-            assert.equal(proofStatus,true);
+            assert.equal(proofStatus[0],true);
         });
 
         it('Should fail for variable storage when encoded value is incorrect',async () =>{
             let proofStatus = await merkleMock.verifyStorage.call("0x468206fb80a036ed801abf5678f1506f1fa61e5ccda1f4de53cc7c", proofJson.storage.variable.path, proofJson.storage.variable.rlpParentNodes, proofJson.storage.variable.storageRoot);
-            assert.equal(proofStatus,false);
+            assert.equal(proofStatus[0],false);
         })
 
         it('Should fail for variable storage when encoded path is incorrect',async () =>{
             let proofStatus = await merkleMock.verifyStorage.call(proofJson.storage.variable.sha3ValueAtStorage, "0x290decd9548b62a8d60345a988386fc84ba6bc95484008f6362f93160ef3e522", proofJson.storage.variable.rlpParentNodes, proofJson.storage.variable.storageRoot);
-            assert.equal(proofStatus,false);
+            assert.equal(proofStatus[0],false);
+        })
+
+        it('Should fail for variable storage when encoded path is empty',async () =>{
+            let proofStatus = await merkleMock.verifyStorage.call(proofJson.storage.variable.sha3ValueAtStorage, "", proofJson.storage.variable.rlpParentNodes, proofJson.storage.variable.storageRoot);
+            assert.equal(proofStatus[0],false);
         })
 
         it('Should fail for variable storage when rlp parent nodes is incorrect',async () =>{
             let proofStatus = await merkleMock.verifyStorage.call(proofJson.storage.variable.sha3ValueAtStorage, proofJson.storage.variable.path, "0xf908cef90211a0f113fc83479a9dcb7a7b5c98cdb42f2426", proofJson.storage.variable.storageRoot);
-            assert.equal(proofStatus,false);
+            assert.equal(proofStatus[0],false);
         })
 
         it('Should fail for variable storage when storage root is incorrect',async () =>{
             let proofStatus = await merkleMock.verifyStorage.call(proofJson.storage.variable.sha3ValueAtStorage, proofJson.storage.variable.path, proofJson.storage.variable.rlpParentNodes, "0xa078cef90211a0f113fc83479a9dcb7a7b5c98cdb42f2426");
-            assert.equal(proofStatus,false);
+            assert.equal(proofStatus[0],false);
         })
 
         it('Should pass for valid mapping storage value',async () =>{
             let proofStatus = await merkleMock.verifyStorage.call(proofJson.storage.mapping.sha3ValueAtStorage, proofJson.storage.mapping.path, proofJson.storage.mapping.rlpParentNodes, proofJson.storage.mapping.storageRoot);
-            assert.equal(proofStatus,true);
+            assert.equal(proofStatus[0],true);
         })
 
         it('Should fail for mapping storage value when encoded value is incorrect',async () =>{
             let proofStatus = await merkleMock.verifyStorage.call("0x468206fb80a036ed801abf5678f1506f1fa61e5ccda1f4de53cc7c", proofJson.storage.mapping.path, proofJson.storage.mapping.rlpParentNodes, proofJson.storage.mapping.storageRoot);
-            assert.equal(proofStatus,false);
+            assert.equal(proofStatus[0],false);
         })
 
         it('Should fail for mapping storage value when encoded path is incorrect',async () =>{
             let proofStatus = await merkleMock.verifyStorage.call(proofJson.storage.mapping.sha3ValueAtStorage, "0x4d6e9a4b1a3576f692c1333198a77a5fb8b72c326f2a4c35eeeaab187773da7a", proofJson.storage.mapping.rlpParentNodes, proofJson.storage.mapping.storageRoot);
-            assert.equal(proofStatus,false);
+            assert.equal(proofStatus[0],false);
         })
 
         it('Should fail for mapping storage value when rlp parent nodes is incorrect',async () =>{
             let proofStatus = await merkleMock.verifyStorage.call(proofJson.storage.mapping.sha3ValueAtStorage, proofJson.storage.mapping.path, "0xf9e08cef90211a0f113fc83479a9dcb7a7b5c98cdb42f2426", proofJson.storage.mapping.storageRoot);
-            assert.equal(proofStatus,false);
+            assert.equal(proofStatus[0],false);
         })
 
         it('Should fail for mapping storage value when storage root is incorrect',async () =>{
             let proofStatus = await merkleMock.verifyStorage.call(proofJson.storage.mapping.sha3ValueAtStorage , proofJson.storage.mapping.path, proofJson.storage.mapping.rlpParentNodes, "0xa078cef90211a0f113fc83479a9dcb7a7b5c98cdb42f2426");
-            assert.equal(proofStatus,false);
+            assert.equal(proofStatus[0],false);
         })
     })
 })
