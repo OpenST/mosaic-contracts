daa1d5289bf3182d3bc25f147a72d7ead131291c
Martin Schenck <martin@ost.com>
2018-09-03 10:59:53 +0200

    Renamed OSTblock to meta-block
    Fixes #311

--- contracts/core/OriginCoreInterface.sol
+++ contracts/core/OriginCoreInterface.sol
@@ -18,29 +18,29 @@ pragma solidity ^0.4.23;
 interface OriginCoreInterface {
 
     /**
-     * @notice Proposes a new OSTblock. The block is stored if the proposal
+     * @notice Proposes a new meta-block. The block is stored if the proposal
      *         succeeds, but its votes still need to be verified in order for
      *         it to be committed.
      *
-     * @param _height Height of the OSTblock in the chain of OSTblocks.
-     * @param _parent The hash of the parent OSTblock.
-     * @param _gas The total consumed gas on auxiliary within this OSTblock.
+     * @param _height Height of the meta-block in the chain of meta-blocks.
+     * @param _parent The hash of the parent meta-block.
+     * @param _gas The total consumed gas on auxiliary within this meta-block.
      * @param _auxiliaryBlockHash The hash of the last finalised checkpoint
-     *                            that is part of this OSTblock.
-     * @param _auxiliaryDynasty The dynasty number where the OSTblock closes on
-     *                          the auxiliary chain.
+     *                            that is part of this meta-block.
+     * @param _auxiliaryDynasty The dynasty number where the meta-block closes
+     *                          on the auxiliary chain.
      * @param _stateRoot The state root of the last finalised checkpoint that
-     *                   is part of this OSTblock.
-     * @param _transactionRoot The transaction root of the OSTblock. A trie
+     *                   is part of this meta-block.
+     * @param _transactionRoot The transaction root of the meta-block. A trie
      *                         created by the auxiliary block store from the
      *                         transaction roots of all blocks.
      * @param _signatureRoot The root of the trie of votes from the last
      *                       finalised checkpoint to its direct child
      *                       checkpoint.
      * @param _depositedValidators Auxiliary addresses of the validators that
-     *                             deposited during the previous OSTblock.
+     *                             deposited during the previous meta-block.
      * @param _loggedOutValidators  Auxiliary addresses of the validators that
-     *                              logged out during the previous OSTblock.
+     *                              logged out during the previous meta-block.
      *
      * @return `true` if the proposal succeeds.
      */
@@ -62,14 +62,14 @@ interface OriginCoreInterface {
     /**
      * @notice Verifies two of the votes that justified the direct child
      *         checkpoint of the last justified auxiliary checkpoint in the
-     *         OSTblock. A supermajority of such votes finalise the last
-     *         auxiliary checkpoint of this OSTblock.
+     *         meta-block. A supermajority of such votes finalise the last
+     *         auxiliary checkpoint of this meta-block.
      *
      * @dev Verifies two votes, as the trie branch includes two leaf nodes in
      *      order to verify all hashes.
      *
-     * @param _ostBlockHash The block hash of the OSTblock for which the votes
-     *                      shall be verified.
+     * @param _metaBlockHash The block hash of the meta-block for which the
+     *                       votes shall be verified.
      * @param _voteTrieBranchRlp The trie branch of the trie of votes from the
      *                           last finalised checkpoint to its direct child
      *                           checkpoint.
@@ -77,7 +77,7 @@ interface OriginCoreInterface {
      * @return `true` if the verification succeeded.
      */
     function verifyVote(
-        bytes32 _ostBlockHash,
+        bytes32 _metaBlockHash,
         bytes _voteTrieBranchRlp
     )
         external
@@ -95,13 +95,13 @@ interface OriginCoreInterface {
         returns (uint256);
 
     /**
-     * @notice Returns the block height of the latest OSTblock that has been
+     * @notice Returns the block height of the latest meta-block that has been
      *         committed.
      *
-     * @dev An OSTblock has been committed if it has been proposed and the
+     * @dev A meta-block has been committed if it has been proposed and the
      *      votes have been verified.
      *
-     * @return The height of the latest committed OSTblock.
+     * @return The height of the latest committed meta-block.
      */
     function latestBlockHeight()
         external
@@ -109,11 +109,11 @@ interface OriginCoreInterface {
         returns (uint256);
 
     /**
-     * @notice Get the state root of an OSTblock.
+     * @notice Get the state root of a meta-block.
      *
      * @param _blockHeight For which block height to get the state root.
      *
-     * @return The state root of the OSTblock.
+     * @return The state root of the meta-block.
      */
     function getStateRoot(
         uint256 _blockHeight
