fad19331627990ef83e1eb3ed3021a5b5571a41c
Gulshan Vasnani <gulshan@ost.com>
2018-09-25 01:34:34 +0530

    Mocked file for test purpose

--- contracts/test/MerklePatriciaProof.sol
+++ contracts/test/MerklePatriciaProof.sol
@@ -9,17 +9,17 @@
 //
 ////import "./RLP.sol";
 //
-//library MerklePatriciaProof {
-//    /*
-//     * @dev Verifies a merkle patricia proof.
-//     * @param value The terminating value in the trie.
-//     * @param encodedPath The path in the trie leading to value.
-//     * @param rlpParentNodes The rlp encoded stack of nodes.
-//     * @param root The root hash of the trie.
-//     * @return The boolean validity of the proof.
-//     */
-////    function verify(bytes32 value, bytes encodedPath, bytes rlpParentNodes, bytes32 root) external pure returns (bool) {
-////        return true;
-////    }
-//
-//}
+library MerklePatriciaProof {
+    /*
+     * @dev Verifies a merkle patricia proof.
+     * @param value The terminating value in the trie.
+     * @param encodedPath The path in the trie leading to value.
+     * @param rlpParentNodes The rlp encoded stack of nodes.
+     * @param root The root hash of the trie.
+     * @return The boolean validity of the proof.
+     */
+    function verify(bytes32 value, bytes encodedPath, bytes rlpParentNodes, bytes32 root) external pure returns (bool) {
+        return true;
+    }
+
+}
