aa149cd219da70f2ea3de42a0b02c39f0f15248c
Deepesh Kumar Nath <deepesh@ost.com>
2018-09-25 23:54:24 +0530

    Updated migration file

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,13 +1,14 @@
 let MessageBus = artifacts.require("./gateway/MessageBus.sol"),
-TestMessageBus = artifacts.require("./gateway/TestMessageBus.sol"),
-Assert = artifacts.require("./Assert.sol");
+    TestMessageBus = artifacts.require("./gateway/TestMessageBus.sol"),
+    Assert = artifacts.require("./Assert.sol"),
+    Gateway = artifacts.require("Gateway");
 
 module.exports = function(deployer) {
-  deployer.deploy(MessageBus);
-  deployer.deploy(Assert);
-  deployer.link(MessageBus, [TestMessageBus]);
-  deployer.link(Assert, [TestMessageBus]);
-  deployer.deploy(TestMessageBus);
+    deployer.deploy(MessageBus);
+    deployer.deploy(Assert);
+    deployer.link(MessageBus, [TestMessageBus, Gateway]);
+    deployer.link(Assert, [TestMessageBus]);
+    deployer.deploy(TestMessageBus);
 };
 
 
