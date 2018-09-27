cbd4a0bf0f05671f183a672ced62fe79f258e403
Gulshan Vasnani <gulshan@ost.com>
2018-09-27 19:29:59 +0530

    Indentation changes

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,4 @@
 let MockMessageBus = artifacts.require("./gateway/MockMessageBus.sol"),
-MockMessage
 TestMessageBus = artifacts.require("./gateway/TestMessageBus.sol"),
 MockMerklePatriciaProof = artifacts.require('./test/MockMerklePatriciaProof'),
 MesssageBusTestWrapper = artifacts.require('./test/MessageBusTestWrapper'),
@@ -8,22 +7,20 @@ MerklePatriciaProof = artifacts.require('./MerklePatriciaProof');
 Assert = artifacts.require("./Assert.sol");
 
 module.exports = function(deployer) {
-  console.log("Before");
-  deployer.deploy(MockMerklePatriciaProof);
-  deployer.link(MockMerklePatriciaProof,MockMessageBus);
-  deployer.deploy(MockMessageBus);
-  deployer.deploy(Assert);
-  deployer.link(MockMessageBus, [TestMessageBus]);
-  deployer.link(Assert, [TestMessageBus]);
-  deployer.deploy(TestMessageBus);
-	
+ 
+	deployer.deploy(MockMerklePatriciaProof);
+	deployer.link(MockMerklePatriciaProof,MockMessageBus);
+	deployer.deploy(MockMessageBus);
+	deployer.deploy(Assert);
+	deployer.link(MockMessageBus, [TestMessageBus]);
+	deployer.link(Assert, [TestMessageBus]);
+	deployer.deploy(TestMessageBus);
 	deployer.link(MockMessageBus, MesssageBusTestWrapper);
 	
 	//For MerklePatricia unit test cases
-  deployer.deploy(MerklePatriciaProof);
+	deployer.deploy(MerklePatriciaProof);
 	deployer.link(MerklePatriciaProof,MerklePatriciaProofTest);
 	deployer.deploy(MerklePatriciaProofTest);
-	console.log("Afterrrrrrr");
 	
 };
 
