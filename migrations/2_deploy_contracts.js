6b310d2f8f293d668a6e6abc1fc0ec90f0ace679
Sarvesh Jain <sarvesh@ost.com>
2018-10-01 16:37:42 +0530

    Removed unused migration , removed assert deploy and link

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,13 +1,7 @@
-let MessageBus = artifacts.require("./gateway/MessageBus.sol"),
-TestMessageBus = artifacts.require("./gateway/TestMessageBus.sol"),
-Assert = artifacts.require("./Assert.sol");
+let MessageBus = artifacts.require("./gateway/MessageBus.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(MessageBus);
-  deployer.deploy(Assert);
-  deployer.link(MessageBus, [TestMessageBus]);
-  deployer.link(Assert, [TestMessageBus]);
-  deployer.deploy(TestMessageBus);
 };
 
 
