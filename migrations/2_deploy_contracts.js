8fa0633212274997d9e62beff861993f270c6282
Martin Schenck <martinschenck@fastmail.com>
2018-12-20 23:05:49 +0100

    Relived TestEIP20Gateway from migrations

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -6,7 +6,6 @@ const MockGatewayLib = artifacts.require("MockGatewayLib");
 const MockGatewayBase = artifacts.require("MockGatewayBase");
 const MetaBlock = artifacts.require("../contracts/lib/MetaBlock.sol");
 const BlockStore = artifacts.require("../contracts/BlockStore.sol");
-const TestEIP20Gateway = artifacts.require("TestEIP20Gateway");
 const EIP20CoGateway = artifacts.require("EIP20CoGateway");
 const TestEIP20CoGateway = artifacts.require("TestEIP20CoGateway");
 const AuxiliaryBlockStore = artifacts.require(
@@ -36,9 +35,9 @@ module.exports = function (deployer) {
     deployer.deploy(GatewayLib);
     deployer.deploy(MockGatewayLib);
     deployer.deploy(MetaBlock);
-    deployer.link(GatewayLib, [GatewayBase, EIP20Gateway, TestEIP20Gateway, EIP20CoGateway, TestEIP20CoGateway]);
-    deployer.link(MessageBus, [EIP20CoGateway, TestEIP20CoGateway, TestEIP20Gateway, EIP20Gateway] );
-    deployer.link(MockGatewayLib, [MockGatewayBase, TestEIP20Gateway]);
+    deployer.link(GatewayLib, [GatewayBase, EIP20Gateway, EIP20CoGateway, TestEIP20CoGateway]);
+    deployer.link(MessageBus, [EIP20CoGateway, TestEIP20CoGateway, EIP20Gateway] );
+    deployer.link(MockGatewayLib, [MockGatewayBase]);
     deployer.link(MetaBlock, [BlockStore, AuxiliaryBlockStore]);
 
     deployer.deploy(MockMerklePatriciaProof);
