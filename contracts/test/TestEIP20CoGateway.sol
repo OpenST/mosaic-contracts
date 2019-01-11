7a5c06f14fa841ace958a4e093bdc95381de8e7c
Gulshan Vasnani <gulshan@ost.com>
2019-01-11 12:41:43 +0530

    Reverted changes in contract

--- contracts/test/TestEIP20CoGateway.sol
+++ contracts/test/TestEIP20CoGateway.sol
@@ -67,32 +67,32 @@ contract TestEIP20CoGateway is EIP20CoGateway {
             _burner
     )
         public
-    { }
+    {}
 
 
     /* Public Functions */
 
     /**
-     * @notice It is used to set the stake message.
+     * @notice It is used to set a message.
      *
      * @dev This is used for testing purpose.
      *
      * @param _intentHash Intent hash.
-     * @param _stakerNonce Nonce of the staker address.
-     * @param _gasPrice Gas price that staker is ready to pay to get the stake
-     *                  and mint process done.
-     * @param _gasLimit Gas limit that staker is ready to pay.
-     * @param _staker Staker address.
+     * @param _nonce Nonce of the message sender address.
+     * @param _gasPrice Gas price that message sender  is ready to pay to
+     *                  transfer message.
+     * @param _gasLimit Gas limit that message sender is ready to pay.
+     * @param _sender Message sender address.
      * @param _hashLock Hash Lock provided by the facilitator.
      *
      * @return messageHash_ Hash unique for every request.
      */
-    function setStakeMessage(
+    function setMessage(
         bytes32 _intentHash,
-        uint256 _stakerNonce,
+        uint256 _nonce,
         uint256 _gasPrice,
         uint256 _gasLimit,
-        address _staker,
+        address _sender,
         bytes32 _hashLock
     )
         public
@@ -100,10 +100,10 @@ contract TestEIP20CoGateway is EIP20CoGateway {
     {
         MessageBus.Message memory message = getMessage(
             _intentHash,
-            _stakerNonce,
+            _nonce,
             _gasPrice,
             _gasLimit,
-            _staker,
+            _sender,
             _hashLock
         );
 
