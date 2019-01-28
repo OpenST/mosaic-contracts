4a4757c432d4788c008a3628ed60b5a37324e9c7
Martin Schenck <martin@ost.com>
2019-01-28 12:11:52 +0100

    Removed loglevel from docker-compose command
    Older versions do not support the option. For example, travis' docker
    compose does not run with the `--loglevel` option.

--- test_integration/main.sh
+++ test_integration/main.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 PROJECT_NAME='mosaic_integration_tests'
-LOGLEVEL='ERROR'
 
-docker-compose --project-name $PROJECT_NAME --log-level $LOGLEVEL up &
+docker-compose --project-name $PROJECT_NAME up &
 truffle --network integration_origin exec integration_tests.js
-docker-compose --project-name $PROJECT_NAME --log-level $LOGLEVEL down
+docker-compose --project-name $PROJECT_NAME down
