d23bb4d49d0eff46d03c2d5304d679bbd2245e22
Martin Schenck <martin@ost.com>
2019-01-30 11:12:21 +0100

    Updated copyright to 2019

--- test/gateway/mock_token_utils.js
+++ test/gateway/mock_token_utils.js
@@ -1,4 +1,4 @@
-// Copyright 2018 OpenST Ltd.
+// Copyright 2019 OpenST Ltd.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
