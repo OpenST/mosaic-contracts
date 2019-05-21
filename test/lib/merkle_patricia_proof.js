aa7916ed96e328c917316da716c9b4f09020aecf
Martin Schenck <martinschenck@fastmail.com>
2019-05-21 14:30:27 +0200

    Add test case that tests all generated proofs
    Tests all cases in parallel using `Promise.all`.
    
    Fixes #719

--- test/lib/merkle_patricia_proof.js
+++ test/lib/merkle_patricia_proof.js
@@ -20,9 +20,13 @@
 
 const MerklePatriciaProof = artifacts.require('./MerklePatriciaProof.sol');
 const proofJson = require('../data/proof.json');
+const generatedProofData = require('../data/generatedProofData.json');
 const Utils = require('../test_lib/utils.js');
 
-contract('MerklePatriciaProof.verify()', (accounts) => {
+/** Converts a buffer that was unmarshalled into a JavaScript object into a hex string. */
+const jsonBufferToHex = json => `0x${Buffer.from(json).toString('hex')}`;
+
+contract('MerklePatriciaProof.verify()', () => {
   let merklePatriciaLib;
 
   describe('Test Cases for Account proof verification', async () => {
@@ -37,7 +41,33 @@ contract('MerklePatriciaProof.verify()', (accounts) => {
         proofJson.account.rlpParentNodes,
         proofJson.account.stateRoot,
       );
-      assert.equal(proofStatus, true);
+      assert.strictEqual(proofStatus, true);
+
+      // Iterate over the stored data set and test all proofs in parallel.
+      // The data set was pre-generated with the fuzzy proof generator (see `./tools`).
+      await Promise.all(
+        generatedProofData.map(
+          async (data) => {
+            const value = jsonBufferToHex(data.value);
+            const path = jsonBufferToHex(data.encodedPath);
+            const parentNodes = jsonBufferToHex(data.rlpParentNodes);
+            const root = jsonBufferToHex(data.root);
+
+            const result = await merklePatriciaLib.verify.call(
+              value,
+              path,
+              parentNodes,
+              root,
+            );
+
+            assert.strictEqual(
+              result,
+              true,
+              `Could not verify data point: ${JSON.stringify(data)}`,
+            );
+          },
+        ),
+      );
     });
 
     it('Should fail when path is incorrect', async () => {
