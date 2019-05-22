bd2b4ef9a893f8c3d5cf04d2cc4c0b9a520577b9
Martin Schenck <martinschenck@fastmail.com>
2019-05-22 11:09:21 +0200

    Store hex instead of a serialized buffer

--- tools/fuzzy_proof_generator_tool/generate.ts
+++ tools/fuzzy_proof_generator_tool/generate.ts
@@ -5,6 +5,28 @@ import FuzzyProofGenerator from './src/FuzzyProofGenerator';
 import ProofData from './src/ProofData';
 import patterns from './patterns';
 
+interface HexProof {
+  value: string;
+  encodedPath: string;
+  rlpParentNodes: string;
+  root: string;
+}
+
+/** Converts a buffer to a hex string with a leading 0x. */
+function bufferToHex(buffer: Buffer): string {
+  return `0x${buffer.toString('hex')}`;
+}
+
+/** Converts a proof to a hex proof. */
+function proofToHexProof(proof: ProofData): HexProof {
+  return {
+    value: bufferToHex(proof.value),
+    encodedPath: bufferToHex(proof.encodedPath),
+    rlpParentNodes: bufferToHex(proof.rlpParentNodes),
+    root: bufferToHex(proof.root),
+  };
+}
+
 const proofs: ProofData[] = [];
 
 // Run 4 times to generate more data.
@@ -13,6 +35,11 @@ for (let i = 0; i < 4; i += 1) {
     (pattern: string): number => proofs.push(FuzzyProofGenerator.generateByPattern(pattern)),
   );
 }
+
+// Convert to hex to be in line with the rest of the code base.
+const hexProofs: HexProof[] = proofs.map(
+  (proof: ProofData): HexProof => proofToHexProof(proof),
+);
 
 const filePath: string = path.join(
   __dirname,
@@ -23,4 +50,4 @@ const filePath: string = path.join(
   'generatedProofData.json',
 );
 
-fs.writeFileSync(filePath, JSON.stringify(proofs));
+fs.writeFileSync(filePath, JSON.stringify(hexProofs));
