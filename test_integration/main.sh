db71033a9f7e4e727d2c64d84a0e161c96906b5b
Deepesh K N <deepesh@ost.com>
2019-05-30 17:23:41 +0530

    Added logs to debug failing tests

--- test_integration/main.sh
+++ test_integration/main.sh
@@ -3,8 +3,12 @@
 PROJECT_NAME='mosaic_integration_tests'
 
 docker-compose --project-name $PROJECT_NAME up &
+docker logs mosaic_integration_tests_geth_node_origin_1
+docker logs mosaic_integration_tests_geth_node_auxiliary_1
 truffle --network integration_origin exec integration_tests.js
 TEST_STATUS=$?
+docker logs mosaic_integration_tests_geth_node_origin_1
+docker logs mosaic_integration_tests_geth_node_auxiliary_1
 docker-compose --project-name $PROJECT_NAME down
 
 exit $TEST_STATUS
