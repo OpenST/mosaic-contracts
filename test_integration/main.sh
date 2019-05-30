dab6ef02eb3fb6b009d7551a09a8cb1569b9f45b
Deepesh K N <deepesh@ost.com>
2019-05-30 19:52:21 +0530

    Removed debug code. Fixed breaking tests

--- test_integration/main.sh
+++ test_integration/main.sh
@@ -3,7 +3,6 @@
 PROJECT_NAME='mosaic_integration_tests'
 
 docker-compose --project-name $PROJECT_NAME up &
-docker ps
 sleep 10
 truffle --network integration_origin exec integration_tests.js
 TEST_STATUS=$?
