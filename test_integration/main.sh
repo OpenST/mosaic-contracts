072f55e423dd78d77f62b0a7ba33586bc0b09a15
Deepesh K N <deepesh@ost.com>
2019-05-30 19:36:10 +0530

    Added sleep after docker compose

--- test_integration/main.sh
+++ test_integration/main.sh
@@ -3,12 +3,9 @@
 PROJECT_NAME='mosaic_integration_tests'
 
 docker-compose --project-name $PROJECT_NAME up &
-docker logs mosaic_integration_tests_geth_node_origin_1
-docker logs mosaic_integration_tests_geth_node_auxiliary_1
+docker ps
+sleep 10
 truffle --network integration_origin exec integration_tests.js
 TEST_STATUS=$?
-docker logs mosaic_integration_tests_geth_node_origin_1
-docker logs mosaic_integration_tests_geth_node_auxiliary_1
 docker-compose --project-name $PROJECT_NAME down
-
 exit $TEST_STATUS
